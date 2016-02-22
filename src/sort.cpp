7a0ffeeb1cfa76a33a5c921b10d8766d4c3cedee
Huang,Liang <liang.huang@transmension.com>
2016-02-22 18:21:19 +0800

    Add insertion sort and selection sort.

--- src/sort.cpp
+++ src/sort.cpp
@@ -1,50 +1,127 @@
 #include "sort.h"
 #include <iostream>
 
-namespace Algorithms {
+#include <time.h>
+#include <stdlib.h>
+#include <algorithm>
 
-    InsertionSort::InsertionSort(int count)
+namespace Algorithms {
+    // Base class of Sort
+    Sort::Sort(int count)
         : COUNT(count)
     {
-        srand(
+        // generate a random shuffled array.
+        srand(time(NULL));
         mNumbers = new int[COUNT];
         for (int i = 0; i < COUNT; i++)
-            mNumbers[i] = rand();
+            mNumbers[i] = i;
+        std::random_shuffle(mNumbers, mNumbers + COUNT);
     }
-    InsertionSort::~InsertionSort()
+    Sort::~Sort()
     {
+        // release array of numbers.
         delete[] mNumbers;
     }
-
-    void InsertionSort::sort()
-    {
-        for (int i = 0; i < COUNT - 1; i++)
-        {
-            for (int j = i + 1; j > 0; j--)
-            {
-                // compare j with j - 1
-                if (mNumbers[j] < mNumbers[j - 1])
-                    exch(j, j - 1);
-                else
-                    break;
-            }
-        }
-    }
-    void InsertionSort::dump()
+    void Sort::dump()
     {
         for (int i = 0; i < COUNT; i++)
             std::cout << mNumbers[i] << " ";
         std::cout << std::endl;
     }
-    void InsertionSort::exch(int i, int j)
+    void Sort::exch(int i, int j)
     {
-        int val = mNumbers[i];
-        mNumbers[i] = mNumbers[j];
-        mNumbers[j] = val;
+        if ( i != j )
+        {
+            int val = mNumbers[i];
+            mNumbers[i] = mNumbers[j];
+            mNumbers[j] = val;
+        }
     }
+
+    // Implementation of InsertionSort
+    InsertionSort::InsertionSort(int count)
+        : mData(count)
+    {
+    }
+    void InsertionSort::sort()
+    {
+        for (int i = 0; i < mData.count() - 1; i++)
+        {
+            for (int j = i + 1; j > 0; j--)
+            {
+                // compare j with j - 1
+                if ( mData.compare(j, j - 1) < 0 )
+                {
+                    mData.exch(j, j - 1);
+                }
+                else
+                {
+                    // array[0]..array[i] are sorted
+                    // array[i + 1] >= array[i], goto next number(i++) to process.
+                    break;
+                }
+                // print out numbers.
+                dump();
+            }
+        }
+    }
+    void InsertionSort::dump()
+    {
+        mData.dump();
+    }
+
+    // Implementation of SelectionSort
+    SelectionSort::SelectionSort(int count)
+        : mData(count)
+    {
+    }
+    void SelectionSort::sort()
+    {
+        for (int i = 0; i < mData.count() - 1; i++)
+        {
+            // q is min val in array[i]..array[N]
+            int q = i;
+            for (int j = i; j < mData.count(); j++)
+            {
+                // select min val in array[i..j]
+                if ( mData.compare(j, q) < 0 )
+                    q = j;
+            }
+            // after this exchange, array[0]..array[i] are sorted.
+            mData.exch(i, q);
+
+            // print out numbers.
+            dump();
+        }
+    }
+    void SelectionSort::dump()
+    {
+        mData.dump();
+    }
+    // Implementation of ShellSort
+    ShellSort::ShellSort(int count)
+        : mData(count)
+    {
+    }
+    void ShellSort::sort()
+    {
+    }
+    void ShellSort::dump()
+    {
+        mData.dump();
+    }
+
 } // end namespace Algorithms
 
 int main()
 {
     std::cout << "algorithms. " << std::endl;
+    Algorithms::InsertionSort sorttest(10);
+//    Algorithms::SelectionSort sorttest(10);
+
+    std::cout << "array before sort" << std::endl;
+    sorttest.dump();
+    sorttest.sort();
+    std::cout << "array after sort" << std::endl;
+    sorttest.dump();
 }
--- src/sort.h
+++ src/sort.h
@@ -2,23 +2,57 @@
 #define __SORT_H__
 
 namespace Algorithms {
+    // Definition of base class.
     class Sort {
-        public:
-
-    };
-    class InsertionSort {
-        // sort arrays using insertion sort.
         private:
             const int COUNT;
             int* mNumbers;
         public:
+            Sort(int count);
+            virtual ~Sort();
+
+            int count() const { return COUNT; }
+            int compare(int i, int j) const
+            {
+                if (mNumbers[i] < mNumbers[j])
+                    return -1;
+                else if (mNumbers[i] == mNumbers[j])
+                    return 0;
+                else return 1;
+            }
+            void exch(int i, int j);
+            void dump();
+    };
+    // Definition of InsertionSort
+    class InsertionSort {
+        // sort arrays using insertion sort.
+        private:
+            Sort mData;
+        public:
             InsertionSort(int count);
-            virtual ~InsertionSort();
 
             void sort();
             void dump();
+    };
+    class SelectionSort {
+        // sort arrays using selection sort.
         private:
-            void exch(int i, int j);
+            Sort mData;
+        public:
+            SelectionSort(int count);
+
+            void sort();
+            void dump();
+    };
+    class ShellSort {
+        // sort arrays using selection sort.
+        private:
+            Sort mData;
+        public:
+            ShellSort(int count);
+
+            void sort();
+            void dump();
     };
 }
 #endif // end of __SORT_H__
